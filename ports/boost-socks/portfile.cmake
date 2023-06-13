330020da9fe04137758830af233f6d755640a55e
LazarusBlake <118471976+LazarusBlake@users.noreply.github.com>
2023-06-14 00:06:13 +0200

    Add boost-socks

---
+++ ports/boost-socks/portfile.cmake
@@ -0,0 +1,10 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO cppalliance/socks
+    REF 713ae06adf39983f90a7d02b2d6ed29038b45284
+    SHA512 98fb62fb97a00cc133417cfc2e1e69f7c62042265770c956345c58294ba148df1900709a0fa3768ad4849d472ab7be6133ef473a8b08db866cd7f96623e4bc1d
+    HEAD_REF develop
+)
+
+include(${CURRENT_INSTALLED_DIR}/share/boost-vcpkg-helpers/boost-modular-headers.cmake)
+boost_modular_headers(SOURCE_PATH ${SOURCE_PATH})
